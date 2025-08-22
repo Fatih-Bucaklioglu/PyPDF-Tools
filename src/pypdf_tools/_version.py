8e03caece09c8cb685cdf0970efa51101bc0cd23
Fatih Bucaklıoğlu <88989455+Fatih-Bucaklioglu@users.noreply.github.com>
2025-08-23 00:58:13 +0300

    Create _version.py

---
+++ src/pypdf_tools/_version.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+PyPDF-Tools Version Information
+Bu dosya setuptools-scm tarafından otomatik olarak güncellenebilir.
+"""
+
+__version__ = "1.0.0"
+__version_tuple__ = (1, 0, 0)
+
+# Build bilgileri (CI/CD tarafından güncellenebilir)
+__build__ = "dev"
+__commit__ = "unknown"
+__date__ = "2024-01-01"
+
+# Uygulama bilgileri
+APP_NAME = "PyPDF-Tools"
+APP_DISPLAY_NAME = "PyPDF Tools"
+APP_DESCRIPTION = "Hibrit PDF Yönetim ve Düzenleme Uygulaması"
+APP_AUTHOR = "Fatih Bucaklıoğlu"
+APP_YEAR = "2024"
+APP_URL = "https://github.com/Fatih-Bucaklioglu/PyPDF-Tools"
+
+# React frontend versiyonu ile uyumluluk kontrolü için
+REACT_BUILD_VERSION = "1.0.0"
+MINIMUM_NODE_VERSION = "16.0.0"
