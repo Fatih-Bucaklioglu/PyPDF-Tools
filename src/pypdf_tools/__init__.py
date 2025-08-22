394da38e98078c3ecc0c73eadea31124ab67ec3d
Fatih Bucaklıoğlu <88989455+Fatih-Bucaklioglu@users.noreply.github.com>
2025-08-23 00:40:53 +0300

    Update __init__.py

--- src/pypdf_tools/__init__.py
+++ src/pypdf_tools/__init__.py
@@ -1,1 +1,12 @@
-a
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+PyPDF-Tools CLI Package
+
+Komut satırı arayüzü ve PDF işleme operasyonları.
+"""
+
+from .cli_handler import cli, cli_main
+
+__all__ = ['cli', 'cli_main']
